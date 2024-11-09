653a0776307a68acdbe20e51bf97b584b924dc38
Nazim <nazim142322@gmail.com>
2024-11-09 16:41:17 +0530

    initial commit

---
+++ Relationship/WebApplication1/WebApplication1/Models/OneToOne/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models.OneToOne
+{
+    public class Employee
+    {
+        public int EmployeeId {get; set;}
+        public String Name {get; set;}  
+        public String Gender { get; set;}
+
+        public EmployeeDetail Details { get; set;} //reference navigation property  
+
+    }
+}
---
+++ Relationship/WebApplication1/WebApplication1/Models/OneToOne/EmployeeDetail.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models.OneToOne
+{
+    public class EmployeeDetail
+    {
+        [Key]
+        public int EmployeeDetailId { get; set; }
+        public string qualification { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public string country { get; set; }
+
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; } 
+        public Employee Employee { get; set; }//reference navigation property
+
+    }
+}
